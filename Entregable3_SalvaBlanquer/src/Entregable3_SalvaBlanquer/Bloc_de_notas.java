ec0f87e34cd7b80110e3ec01a4dd9f5b777e6864
Salva DAM <sblanquer@campuscamarafp.com>
2021-01-08 13:28:02 +0100

    Progreso en la entrega3

--- Entregable3_SalvaBlanquer/src/Entregable3_SalvaBlanquer/Bloc_de_notas.java
+++ Entregable3_SalvaBlanquer/src/Entregable3_SalvaBlanquer/Bloc_de_notas.java
@@ -4,6 +4,7 @@
 package Entregable3_SalvaBlanquer;
 
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
@@ -11,6 +12,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import Entregable3_SalvaBlanquer.Tiempo_Tareas;
 
 /**
  * @author Salvador
@@ -24,13 +26,11 @@ public class Bloc_de_notas {
 	public static void main(String[] args) {
 
 		Scanner teclado = new Scanner(System.in);// transcibe lo que escribas en el teclado al terminal
-		Date date = new Date();
-
-		DateFormat hourdateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss ");
-
+		
+		Tareas tareas1 = null;
 		boolean repite;
 
-		while (repite = true) {
+		while (repite = true) { 
 			System.out.println("");
 			System.out.println("Escoga una opcion ");
 			System.out.println("");
@@ -45,9 +45,21 @@ public class Bloc_de_notas {
 
 			case 1:
 				System.out.println("titulo de la nueva tarea");
+				
+			 tareas1= CrearTarea();
+			 
+			 try {
+					File myFile = new File("src\\Entregable3_SalvaBlanquer\\Tareas");
 
-				teclado.nextLine();
-
+							
+					PrintWriter pw = new PrintWriter(new FileWriter(myFile, true)); //Escritura del documento
+					pw.println(tareas1);
+					
+					pw.close();
+			 } catch (Exception e) { // Control de excepciones
+					System.out.println("Revisa el codigo");
+					e.printStackTrace(); // imprime el error
+				}
 				break;
 
 			case 2:
@@ -65,30 +77,43 @@ public class Bloc_de_notas {
 			case 4:
 				System.out.println("Hasta Luego");
 				repite = false;
-				
+				return;
 				
 			}
 
 			try {
-				File myFile = new File("C:\\Users\\salva\\Desktop\\de salva\\Programas Camarafp\\Programas Eclipse\\Entregable3_Salva_Blanquer\\Entregable3\\Entregable3_SalvaBlanquer\\src\\Entregable3_SalvaBlanquer\\Tareas");
+				File myFile = new File("src\\Entregable3_SalvaBlanquer\\Tareas");
 
 						
 				PrintWriter pw = new PrintWriter(new FileWriter(myFile, true)); //Escritura del documento
-				pw.println(teclado.nextLine() + hourdateFormat.format(date));
-				System.out.println("");
+				pw.println(tareas1);
+				
 				pw.close();
     
 				Scanner doc = new Scanner(myFile);// Lectura del documento
 				while (doc.hasNextLine()) {
 					String Tareas = doc.nextLine();
-
+					System.out.println(Tareas);
+					
 				}
 			} catch (Exception e) { // Control de excepciones
 				System.out.println("Revisa el codigo");
 				e.printStackTrace(); // imprime el error
-			}
-
-		}
+			}}
+			
+		
 
 	}
+
+
+
+public static Tareas CrearTarea() {
+	Scanner sc = new Scanner(System.in);
+	String Titulo = sc.nextLine();
+	boolean Estado = false;
+	Date fecha = new Date();
+	return new Tareas(Titulo,fecha,Estado);
+
 }
+}
+
